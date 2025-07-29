577fd52725974fd5ca974825e320a49a61ed871d
shree <mudabeshrinivas@gmail.com>
2025-07-29 23:24:13 +0530

    Change 7

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -8,7 +8,7 @@ import {
   ArrowTrendingUpIcon,
   ChartBarIcon
 } from '@heroicons/react/24/outline';
-import { useApp } from '../../context/AppContext';
+import { useApp } from '../context/AppContext';
 
 const Dashboard = () => {
   const { tasks, setActivePage } = useApp();
